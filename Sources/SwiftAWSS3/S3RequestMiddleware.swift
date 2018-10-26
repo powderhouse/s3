fa29eb284fb74c6a1fc6061bc28fd5db71ce1177
Jared Cosulich <jared.cosulich@gmail.com>
2018-10-26 11:31:58 -0400

    updating S3 Middleware to handle localhost properly

--- Sources/SwiftAWSS3/S3RequestMiddleware.swift
+++ Sources/SwiftAWSS3/S3RequestMiddleware.swift
@@ -4,43 +4,46 @@ import AWSSDKSwiftCore
 struct S3RequestMiddleware: AWSRequestMiddleware {
     func chain(request: AWSRequest) throws -> AWSRequest {
         var request = request
-        
+
         var paths = request.url.path.components(separatedBy: "/").filter({ $0 != "" })
         if paths.count == 0 {
             return request
         }
-        
-        switch request.httpMethod.lowercased() {
-        case "get":
-            let query = request.url.query != nil ? "?\(request.url.query!)" : ""
-            let domain: String
-            if let host = request.url.host, host.contains("amazonaws.com") {
-                domain = "s3-\(request.region.rawValue).amazonaws.com"
-            } else {
-                let port = request.url.port == nil ? "" : ":\(request.url.port!)"
-                domain = request.url.host!+port
-            }
-            request.url = URL(string: "\(request.url.scheme ?? "https")://\(paths.removeFirst()).\(domain)/\(paths.joined(separator: "/"))\(query)")!
-        default:
-            guard let host = request.url.host, host.contains("amazonaws.com") else { break }
-            var pathes = request.url.path.components(separatedBy: "/")
-            if paths.count > 1 {
-                _ = pathes.removeFirst() // /
-                let bucket = pathes.removeFirst() // bucket
-                var urlString: String
-                if let firstHostComponent = host.components(separatedBy: ".").first, bucket == firstHostComponent {
-                    // Bucket name is part of host. No need to append bucket
-                    urlString = "https://\(host)/\(pathes.joined(separator: "/"))"
+        if let host = request.url.host, !host.contains("localhost") {
+            switch request.httpMethod.lowercased() {
+            case "get":
+                let query = request.url.query != nil ? "?\(request.url.query!)" : ""
+                let domain: String
+                if host.contains("amazonaws.com") {
+                    domain = "s3-\(request.region.rawValue).amazonaws.com"
                 } else {
-                    urlString = "https://\(bucket).\(host)/\(pathes.joined(separator: "/"))"
+                    let port = request.url.port == nil ? "" : ":\(request.url.port!)"
+                    domain = request.url.host!+port
                 }
-                if let query = request.url.query {
-                    urlString += "?\(query)"
+
+                request.url = URL(string: "\(request.url.scheme ?? "https")://\(paths.removeFirst()).\(domain)/\(paths.joined(separator: "/"))\(query)")!
+
+            default:
+                guard host.contains("amazonaws.com") else { break }
+                var pathes = request.url.path.components(separatedBy: "/")
+                if paths.count > 1 {
+                    _ = pathes.removeFirst() // /
+                    let bucket = pathes.removeFirst() // bucket
+                    var urlString: String
+                    if let firstHostComponent = host.components(separatedBy: ".").first, bucket == firstHostComponent {
+                        // Bucket name is part of host. No need to append bucket
+                        urlString = "https://\(host)/\(pathes.joined(separator: "/"))"
+                    } else {
+                        urlString = "https://\(bucket).\(host)/\(pathes.joined(separator: "/"))"
+                    }
+                    if let query = request.url.query {
+                        urlString += "?\(query)"
+                    }
+                    request.url = URL(string: urlString)!
                 }
-                request.url = URL(string: urlString)!
             }
         }
-        
+
         switch request.operation {
         case "CreateBucket":
             var xml = ""
@@ -50,7 +53,7 @@ struct S3RequestMiddleware: AWSRequestMiddleware {
             xml += "</LocationConstraint>"
             xml += "</CreateBucketConfiguration>"
             request.body = .text(xml)
-            
+
         case "PutObject":
             if let data = try request.body.asData() {
                 let encoded = Data(bytes: md5(data)).base64EncodedString()
@@ -59,7 +62,7 @@ struct S3RequestMiddleware: AWSRequestMiddleware {
         default:
             break
         }
-        
+
         return request
     }
 }
